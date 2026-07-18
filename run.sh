303820571262dd6d924c9a9533fefa31764e45f5
Azagal258 <azagal.minecraft@gmail.com>
2026-07-18 19:22:22 +0200

    make small adjustements

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# moves wherever the user saved the code
 cd "$(dirname "$0")" || exit 1
 
+# starts venv
 source .venv/bin/activate
 python script.py 2>&1 | tee run.log
 
+# Prompts infos to user
 echo
-echo "Program finished. Press Enter to close."
-read
+echo "Program finished."
+read -r -p "Press Enter to close..."
--- setup.sh
+++ setup.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+# moves wherever the user saved the code
 cd "$(dirname "$0")" || exit 1
 
+# updates the code
+git pull
+
+# starts venv
 python3 -m venv .venv
 source .venv/bin/activate
 
+# installs packages
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
-chmod +x ~/Bifrost/run.sh
+# makes run executable
+chmod +x ./run.sh
 
-echo "Setup complete. Run:"
-echo "python3 ~/Bifrost/script.py"
+# Prompts infos to user
+echo "Setup/Update complete."
+echo "You can now run the script by double-clicking run.sh"
+read -r -p "Press Enter to close..."
